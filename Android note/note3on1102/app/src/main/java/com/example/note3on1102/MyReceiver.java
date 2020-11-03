74c9d678ba191ce46437031d190c7b22d8ce9527
Suzie Lee <wnsuwls0707@gmail.com>
2020-11-03 23:23:04 +0900

    [Add] 1102 assignment 3 finished

--- Android note/note3on1102/app/src/main/java/com/example/note3on1102/MyReceiver.java
+++ Android note/note3on1102/app/src/main/java/com/example/note3on1102/MyReceiver.java
@@ -3,6 +3,7 @@ package com.example.note3on1102;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
     @Override
